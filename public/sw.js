bd712c2d7ba8be61284b999f5bd5132f64bfaa65
aabhiyann <sainjuabhiyan321@gmail.com>
2026-03-06 18:11:45 -0500

    feat: PWA manifest and service worker

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+/* SnapMacros PWA service worker - minimal install/activate */
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
